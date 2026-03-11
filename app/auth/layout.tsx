f4ab8fdc751058508d864e2e6734a61d9e1ca39a
wintermute08 <gimjiwan009@gmail.com>
2026-03-11 10:13:50 +0900

    TypeScript 빌드 오류 수정

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,9 +1,9 @@
-export default function AuthLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="min-h-screen bg-white flex flex-col items-center justify-center px-5 pb-10">
-      <div className="w-full max-w-[400px]">
-        {children}
-      </div>
-    </div>
-  )
+export const dynamic = 'force-dynamic'
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return <>{children}</>
 }

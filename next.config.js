1b3ad07912dc1f7080f7997816e8b1b93aceb31e
wintermute08 <gimjiwan009@gmail.com>
2026-03-11 12:58:10 +0900

    fix: add manifest.json, favicon.ico, allow cdn.jsdelivr.net in CSP

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ const nextConfig = {
             key: "Content-Security-Policy",
             value: [
               "script-src 'self' 'unsafe-eval' 'unsafe-inline' https:;",
-              "connect-src 'self' https://*.supabase.co https://*.supabase.in wss://*.supabase.co;",
+              "connect-src 'self' https://*.supabase.co https://*.supabase.in wss://*.supabase.co https://cdn.jsdelivr.net;",
               "frame-src 'self' https://*.supabase.co https://*.supabase.in https://accounts.google.com https://github.com https://api.github.com;",
               "img-src 'self' data: blob: https:;",
               "style-src 'self' 'unsafe-inline' https:;",
